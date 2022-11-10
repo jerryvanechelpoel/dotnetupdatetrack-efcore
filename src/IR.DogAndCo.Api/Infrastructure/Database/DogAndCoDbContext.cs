720befba72fe95de0d83dfe7d43092cd219ecc48
Jerry Van Echelpoel <Jerry.VanEchelpoel@realdolmen.com>
2022-11-10 14:59:44 +0100

    Add DbSet properties with configuration

---
+++ src/IR.DogAndCo.Api/Infrastructure/Database/DogAndCoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IR.DogAndCo.Api;
+
+public sealed class DogAndCoDbContext : DbContext
+{
+    public DbSet<CustomerEntity> Customers { get; set; }
+    public DbSet<OrderEntity> Orders { get; set; }
+    public DbSet<ProductEntity> Products { get; set; }
+    public DbSet<ProductTypeEntity> ProductTypes { get; set; }
+    public DbSet<PromotionEntity> Promotions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
